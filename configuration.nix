a999b507f0b7e216e3c574632b0231076573adda
yhsuh <yhsuh@mpcdf.mpg.de>
2026-04-23 21:56:27 +0000

    initial:

---
+++ configuration.nix
@@ -0,0 +1,55 @@
+{ config, lib, pkgs, ... }:
+
+{
+  wsl.enable = true;
+  wsl.defaultUser = "yhsuh";
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Use OpenSSH with Kerberos/GSSAPI support
+  programs.ssh.package = pkgs.opensshWithKerberos;
+
+  # Shell aliases
+  programs.bash.shellAliases = {
+    nrs = "sudo nixos-rebuild switch --flake ~/nix-config#nixos";
+    nrt = "sudo nixos-rebuild test --flake ~/nix-config#nixos";
+  };
+
+  # System packages
+  environment.systemPackages = with pkgs; [
+    git
+  ];
+
+  # Kerberos for MPCDF GSSAPI authentication
+  security.krb5 = {
+    enable = true;
+    settings = {
+      libdefaults = {
+        default_realm = "IPP-GARCHING.MPG.DE";
+        forwardable = true;
+      };
+      realms."IPP-GARCHING.MPG.DE" = {
+        kdc = [
+          "kerberos.rzg.mpg.de"
+          "kerberos1.rzg.mpg.de"
+          "kerberos2.rzg.mpg.de"
+          "kerberos3.rzg.mpg.de"
+        ];
+        admin_server = "kerberos1.rzg.mpg.de";
+        default_domain = "rzg.mpg.de";
+      };
+      domain_realm = {
+        "mpcdf.mpg.de" = "IPP-GARCHING.MPG.DE";
+        ".mpcdf.mpg.de" = "IPP-GARCHING.MPG.DE";
+        "rzg.mpg.de" = "IPP-GARCHING.MPG.DE";
+        ".rzg.mpg.de" = "IPP-GARCHING.MPG.DE";
+        "ipp.mpg.de" = "IPP-GARCHING.MPG.DE";
+        ".ipp.mpg.de" = "IPP-GARCHING.MPG.DE";
+        "ipp-garching.mpg.de" = "IPP-GARCHING.MPG.DE";
+        ".ipp-garching.mpg.de" = "IPP-GARCHING.MPG.DE";
+      };
+    };
+  };
+
+  system.stateVersion = "25.11";
+}
